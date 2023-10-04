9d069642cdcfcb1d0d49e37608089f8a03c8beb6
shantisuresh01 <shantisuresh01@gmail.com>
2023-10-04 16:48:30 -0400

    Added code for the name guard, import statements and f-strings.

--- day1_intro/day1_strings_ranges.py
+++ day1_intro/day1_strings_ranges.py
@@ -1,92 +1,101 @@
-# # Screen output
-# first_name:str = "Fred"
-# print(first_name)
-#
-# # Extend a string
-# first_name = 'Fred' + " " + "O'Connor"
-# print(first_name)
-#
-# # Re-assigning and "int" type to a previously declared "str" type
-# # Python allows this as it is a dynamically types language.
-# first_name:int = 1
-# print(first_name)
-#
-# # Initializing variables
-# my_first_int: int = 3
-# print(my_first_int)
-#
-# # Exploring backslash
-# # Screen output
-# first_name:str = "Fred \\ \n"
-# print(first_name)
-#
-# # Exploring f-string
-# print(f'My first integer is: {my_first_int}')
-#
-# str1 = 'xyz123'
-# strlen = len(str1) # 'len()' is a built-in function
-# print(strlen)
-#
-# # Exploring string positions
-# str1 = 'abcdef'
-# print(str1[0], str1[1])
-#
-# str = 'abcdefghi'
-# print(str[2:6])
+# main utility function
+def very_useful_utility():
+    # print(f"My __name__ :  {__name__}")
 
-# str = 'abcdefghijklmno'
-# print(str[3:11:2])
+    # # Screen output
+    # first_name:str = "Fred"
+    # print(first_name)
+    #
+    # # Extend a string
+    # first_name = 'Fred' + " " + "O'Connor"
+    # print(first_name)
+    #
+    # # Re-assigning and "int" type to a previously declared "str" type
+    # # Python allows this as it is a dynamically types language.
+    # first_name:int = 1
+    # print(first_name)
+    #
+    # # Initializing variables
+    # my_first_int: int = 3
+    # print(my_first_int)
+    #
+    # # Exploring backslash
+    # # Screen output
+    # first_name:str = "Fred \\ \n"
+    # print(first_name)
+    #
+    # # Exploring f-string
+    # print(f'My first integer is: {my_first_int}')
+    #
+    # str1 = 'xyz123'
+    # strlen = len(str1) # 'len()' is a built-in function
+    # print(strlen)
+    #
+    # # Exploring string positions
+    # str1 = 'abcdef'
+    # print(str1[0], str1[1])
+    #
+    # str = 'abcdefghi'
+    # print(str[2:6])
 
-# str = 'abcdefghi'
-# last = str[-1]
-# print(last)
-# print(str[-2])
-#
-# str = 'abcdefghi'
-# print(str[0:-2])
-# print(str[:])
-# print(str[::-2])
-# print(str[::2])
+    # str = 'abcdefghijklmno'
+    # print(str[3:11:2])
 
-# str = 'abcdefghi'
-# print(str[-4:-2])
+    # str = 'abcdefghi'
+    # last = str[-1]
+    # print(last)
+    # print(str[-2])
+    #
+    # str = 'abcdefghi'
+    # print(str[0:-2])
+    # print(str[:])
+    # print(str[::-2])
+    # print(str[::2])
 
-# bigstr = 'abc' * 3
-# print(bigstr)
-# divider = '=' * 30
-# print(divider)
-# str = 'First Name\tLast Name'
-# print(str)
-#
-# str1 = """Hello everybody
-# this is a long string that
-# takes up multiple lines!
-# We use triple-quotes for it!"""
-# print(str1)
+    # str = 'abcdefghi'
+    # print(str[-4:-2])
 
-# str1 = r'I need a backslash\in this string. And the \t does not make a tab!'
-# str1 = r'\Users\john\earnings.xlsx'
-# print(str1)
+    # bigstr = 'abc' * 3
+    # print(bigstr)
+    # divider = '=' * 30
+    # print(divider)
+    # str = 'First Name\tLast Name'
+    # print(str)
+    #
+    # str1 = """Hello everybody
+    # this is a long string that
+    # takes up multiple lines!
+    # We use triple-quotes for it!"""
+    # print(str1)
 
-str = 'hello'
-print(str + ' world!')
-str = str + ' world!'
-print(str + "yay it worked!")
+    # str1 = r'I need a backslash\in this string. And the \t does not make a tab!'
+    # str1 = r'\Users\john\earnings.xlsx'
+    # print(str1)
 
-# immutable: str, int, float, tuple
-# mutable: l = list ['a', 'b', 'c']
-# l.extend("d")
-# l looks like this ['a', 'b', 'c', 'd']
+    str = 'hello'
+    print(str + ' world!')
+    str = str + ' world!'
+    print(str + "yay it worked!")
 
-# h e l l o
-# h e l l o w o r l d
+    # immutable: str, int, float, tuple
+    # mutable: l = list ['a', 'b', 'c']
+    # l.extend("d")
+    # l looks like this ['a', 'b', 'c', 'd']
+
+    # h e l l o
+    # h e l l o w o r l d
 
 
-# division: /
-# backslash: \\
+    # division: /
+    # backslash: \\
 
 
-# Getting input from the user
-# prompt:str = 'What is your name? '
-# resp = input(prompt)
-# print(resp)
+    # Getting input from the user
+    # prompt:str = 'What is your name? '
+    # resp = input(prompt)
+    # print(resp)
+
+# Set up Name guard
+if __name__ == "__main__":
+    # run all commands inside that function
+    very_useful_utility()
--- day1_intro/introduction.py
+++ day1_intro/introduction.py
@@ -1,19 +1,21 @@
 # Screen output
-import day1_strings_ranges
+import day1_intro.day1_strings_ranges
+import day1_intro.index_work
 
-first_name = 'Fred'
-print(first_name)
+if __name__ == "__main__":
+    first_name = 'Fred'
+    print(first_name)
 
-# Initializing variables
-my_first_var: int = 3
-print(my_first_var)
+    # Initializing variables
+    my_first_var: int = 3
+    print(my_first_var)
 
-# Getting inpout from the user
-prompt = 'What is your name? '
-n = input(prompt)
-print(n)
+    # Getting inpout from the user
+    prompt = 'What is your name? '
+    n = input(prompt)
+    print(n)
 
-day1_strings_ranges.very_useful_utility()
-print(f"__name__ for day1_strings_ranges: {day1_strings_ranges.__name__}")
-print(f"My __name__ :  {__name__}")
+    day1_intro.day1_strings_ranges.very_useful_utility()
+    print(f"__name__ for day1_strings_ranges: {day1_intro.day1_strings_ranges.__name__}")
+    print(f"My __name__ :  {__name__}")
 
